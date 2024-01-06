dbc016b7cf93cd1e944f151cda2bea8525c7e6b8
AndDev <anddesenvolved@gmail.com>
2024-01-06 09:15:58 -0300

    Perfil data acess finished

--- src/views/pages/dashboard/perfil/perfilSlice.js
+++ src/views/pages/dashboard/perfil/perfilSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+//Criar lógica para armazenar o email no navegador;
 const initialState = {
     name: '',
-    link_img: '',
     text: '',
     author: '',
     constance: 0,
+    img_link: '',
     status: 'idle',
     error: null,
 }
@@ -16,9 +17,25 @@ const perfilSlice = createSlice({
         nameEnter(state, action){
             const name = action.payload;
             state.name = name;
-        }
+        },
+        textEnter(state, action){
+            const text = action.payload;
+            state.text = text;
+        },
+        authorEnter(state, action){
+            const author = action.payload;
+            state.author = author;
+        },
+        constanceEnter(state, action){
+            const constance = action.payload;
+            state.constance = constance;
+        },
+        img_linkEnter(state, action){
+            const img_link = action.payload;
+            state.img_link = img_link;
+        },
     }
 })
 
-export const { nameEnter } = perfilSlice.actions;
+export const { nameEnter, textEnter, authorEnter, constanceEnter, img_linkEnter } = perfilSlice.actions;
 export default perfilSlice.reducer;
