5ddb883698e2131ac1cee50c3bf3f899d1753f19
AndDev <anddesenvolved@gmail.com>
2024-01-15 12:30:30 -0300

    habits data fecth api

--- habits/habitsDAO.php
+++ habits/habitsDAO.php
@@ -7,6 +7,18 @@ class habitsDAO extends BasicDAO{
         $sql = "INSERT INTO habits VALUES(default, ?, ?, ?, ?, ?, ?, ?)";
         $this->execDML($sql, $user_id, $name, $importance, $dificulty, $category, $weekDays, $description);
     }
+
+    public function getHabitsData($id){
+        $sql = "SELECT * FROM habits WHERE user_id = ?";
+        $pdo = $this->getConnection();
+        try{
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute($id);
+            return $stmt->fetchAll();
+        }finally{
+            $pdo = null;
+        }
+    }
 }
 
 ?>
